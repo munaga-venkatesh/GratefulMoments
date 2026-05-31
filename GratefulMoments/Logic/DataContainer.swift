82c15699e76b8c480a4c6267a979056a66030e70
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-31 21:08:39 +0530

    add swiftData for Moments

---
+++ GratefulMoments/Logic/DataContainer.swift
@@ -0,0 +1,8 @@
+//
+//  DataContainer.swift
+//  GratefulMoments
+//
+//  Created by Venkatesh Munaga on 31/05/26.
+//
+
+import Foundation
---
+++ GratefulMoments/Models/Moment.swift
@@ -0,0 +1,8 @@
+//
+//  Moment.swift
+//  GratefulMoments
+//
+//  Created by Venkatesh Munaga on 31/05/26.
+//
+
+import Foundation
