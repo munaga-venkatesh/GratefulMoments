b9ed1bd774317f63c21aa972bf6813fd14b0c9a8
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-31 19:20:11 +0530

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Venkatesh Munaga on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
