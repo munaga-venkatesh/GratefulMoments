c7f689f0482322d65ec34e8ed26ccfae63abe0ec
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-31 21:09:14 +0530

    added SwiftData for Moments

--- GratefulMoments/GratefulMomentsApp.swift
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct GratefulMomentsApp: App {
+    let dataContiner = DataContainer()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(dataContiner)
         }
+        .modelContainer(dataContiner.modelContainer)
     }
 }
--- GratefulMoments/Logic/DataContainer.swift
+++ GratefulMoments/Logic/DataContainer.swift
@@ -5,4 +5,54 @@
 //  Created by Venkatesh Munaga on 31/05/26.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+@Observable
+@MainActor
+class DataContainer {
+    let modelContainer: ModelContainer
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    init(includeSampleMoments: Bool = false) {
+        let schema = Schema([
+            Moment.self,
+        ])
+        
+        let modelConfiguration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: includeSampleMoments
+        )
+        
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            
+            if includeSampleMoments {
+                loadSampleMoments()
+            }
+            try context.save()
+        } catch {
+            fatalError("Could not create model container: \(error)")
+        }
+    }
+    
+    private func loadSampleMoments() {
+        for moment in Moment.sampleData {
+            context.insert(moment)
+        }
+    }
+}
+
+
+private let sampleContainer: DataContainer = .init(includeSampleMoments: true)
+
+extension View {
+    func sampleDataContainer() -> some View {
+        self
+            .environment(sampleContainer)
+            .modelContainer(sampleContainer.modelContainer)
+    }
+}
